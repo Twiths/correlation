a98c4423d5958b39af076b87fb14fdf57c01b160
Kally <kallyasmedia@gmail.com>
2021-01-01 12:59:31 +0000

    update for loop

--- corelation.c
+++ corelation.c
@@ -12,23 +12,30 @@ int main() {
 
   puts("How many values do you have?");
   scanf("%d", &n);
-  puts("Enter x values");
+  puts("Enter x and y values");
   for (int i = 0; i <= n; i++) {
+    printf("X value [%d]: ", i);
     scanf("%d", &xVaues[i]);
     xSum = xSum + xVaues[i];
     xSumSq = squareNumber(xSum);
-  }
 
-  puts("Enter y Values");
-  for (int i = 0; i <= n; i++) {
+    // y Values
+    printf("Y value [%d]: ", i);
     scanf("%d", &yVaues[i]);
     ySum = ySum + yVaues[i];
     ySumSq = squareNumber(ySum);
   }
+
+  //   puts("Enter y Values");
+  //   for (int i = 0; i <= n; i++) {
+  //     scanf("%d", &yVaues[i]);
+  //     ySum = ySum + yVaues[i];
+  //     ySumSq = squareNumber(ySum);
+  //   }
   y = a * x + b;
   //   Print the output here
   printf("Sum of X values is: %d\n", xSum);      // Print sumation of x values
-  printf("Sum of X values is: %d", ySum);        // Print sumation of x values
+  printf("Sum of Y values is: %d\n", ySum);      // Print sumation of x values
   printf("Square of Y values is: %d\n", ySumSq); // Print Square of Y values
   return 0;
 }
